6cdd152d9b18cb3f7e70a15b10fd00b0084b55e9
Kaveri <846336@cognizant.com>
2020-02-21 15:44:26 +0530

    'Commit'

--- About.js
+++ About.js
@@ -2,28 +2,16 @@ import React, { Component } from 'react';
 import Navcomp from './Navcomp';
 import {Link} from 'react-router-dom'
  class About extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//          showMe:true
-//     }
-// }
-// Buttondata=()=>{
-// this.setState({
-//   showMe:false
-// })
-// }
+
     render() {
       
-        console.log('88888888888888888888888888888888888888888888')
+        console.log('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
         return (
             <div>
                 It Is About Component 
                 <br/>
                 <button type="button" onClick={this.props.clichandler} class="btn bg-primary"><Link  to='/Button' class="text-white">Click Me</Link></button>
-               {/* {
-                 this.state.showMe?<Navcomp/>:null
-               } */}
+            
             </div>
         )
     }
